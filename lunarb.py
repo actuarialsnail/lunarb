a4561370303cf9b570426663249f943fe244982e
actuarialsnail <flynn.yiyi@gmail.com>
2022-03-22 23:20:56 +0800

    lunax pairs

--- lunarb.py
+++ lunarb.py
@@ -1,7 +1,11 @@
+# pip install terra_sdk
+from terra_sdk.client.lcd import LCDClient
 import requests
 import json
+import time
 
-from terra_sdk.client.lcd import LCDClient
+starttime = time.time()
+
 
 terra = LCDClient(
     chain_id="columbus-5",
@@ -18,6 +22,8 @@ pairs = {
     "luna_lunax_terreswap": "terra1zrzy688j8g6446jzd88vzjzqtywh6xavww92hy",
     "luna_bluna_loop": "terra1v93ll6kqp33unukuwls3pslquehnazudu653au",
     "luna_lunax_loop": "terra1ga8dcmurj8a3hd4vvdtqykjq9etnw5sjglw4rg",
+    "luna_bluna_astroport": "terra1j66jatn3k50hjtg2xemnjm8s7y8dws9xqa5y8w",
+    "luna_lunax_astroport": "terra1qswfc7hmmsnwf7f2nyyx843sug60urnqgz75zu"
 }
 
 query_msg = json.dumps({
@@ -29,10 +35,25 @@ query_msg = json.dumps({
     }
 })
 
-for p in pairs:
-    response = requests.get(
-        "https://lcd.terra.dev/wasm/contracts/" + pairs[p] + "/store",
-        params={"query_msg": query_msg},
+while True:
+
+    stader_res = requests.get(
+        'https://fcd.terra.dev/terra/wasm/v1beta1/contracts/terra1xacqx447msqp46qmv8k2sq6v5jh9fdj37az898/store',
+        params={"query_msg": "eyJzdGF0ZSI6e319"}
     ).json()
-    price = int(response["result"]["return_amount"])/1000000 - 1
-    print(p, '{:.2f}%'.format(price*100))
+
+    stader_unbond_price = float(
+        stader_res["query_result"]["state"]["exchange_rate"]) - 1
+
+    print("LunaX-Luna unbond", '{:.2f}%'.format(stader_unbond_price*100))
+
+    for p in pairs:
+        response = requests.get(
+            "https://lcd.terra.dev/wasm/contracts/" + pairs[p] + "/store",
+            params={"query_msg": query_msg},
+        ).json()
+        price = int(response["result"]["return_amount"])/1000000 - 1
+        price = price + stader_unbond_price if "lunax" in p else price
+        print(p, '{:.2f}%'.format(price*100))
+
+    time.sleep(5.0 - ((time.time() - starttime) % 5.0))
